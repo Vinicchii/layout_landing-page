830f5cb1f288dbaffab3be0e8c79a28d568014da
Vinicius Maximiano <v.maximiano@hotmail.com>
2026-03-28 02:48:36 -0300

    feat: header and hero added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+const phoneIcon = document.querySelector('.header__content__info__phone');
+const tooltip = document.querySelector('.header__content__info__tooltip');
+
+phoneIcon.addEventListener('mouseenter', () => {
+  tooltip.style.opacity = '1';
+  tooltip.style.visibility = 'visible';
+});
+
+phoneIcon.addEventListener('mouseleave', () => {
+  tooltip.style.opacity = '0';
+  tooltip.style.visibility = 'hidden';
+});

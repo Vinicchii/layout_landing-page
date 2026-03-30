a0af238dbb834a1af9076d7a683d80c2b5fb1491
Vinicius Maximiano <v.maximiano@hotmail.com>
2026-03-29 23:17:44 -0300

    feat: header menu added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,12 @@
 'use strict';
+
 const phoneIcon = document.querySelector('.header__content__info__phone');
+
 const tooltip = document.querySelector('.header__content__info__tooltip');
+
+const menuIcon = document.querySelector('.header__content__info__menu');
+
+const closeIcon = document.querySelector('.header__menu__top__close');
 
 phoneIcon.addEventListener('mouseenter', () => {
   tooltip.style.opacity = '1';
@@ -10,4 +16,16 @@ phoneIcon.addEventListener('mouseenter', () => {
 phoneIcon.addEventListener('mouseleave', () => {
   tooltip.style.opacity = '0';
   tooltip.style.visibility = 'hidden';
+});
+
+menuIcon.addEventListener('click', () => {
+  const header = document.querySelector('.header__menu');
+
+  header.style.display = 'block';
+});
+
+closeIcon.addEventListener('click', () => {
+  const header = document.querySelector('.header__menu');
+
+  header.style.display = 'none';
 });
